be41a89b2ddd6d7fca872d42bdece0e01f598da0
Deinyefa Eporwei <df.eporwei@gmail.com>
2016-09-29 19:24:33 -0400

    Laser Defender..

---
+++ Assets/Scripts/SpaceshipController.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SpaceshipController : MonoBehaviour {
+
+	public Done_GameController gameController;
+
+	public float moveSpeed = 10f;
+
+	private Rigidbody rb;
+
+	void Start () {
+		rb = GetComponent<Rigidbody> ();
+	}
+	
+	void FixedUpdate () {
+		rb.MovePosition (transform.position + Vector3.forward * moveSpeed * Time.deltaTime);
+	}
+
+	void OnTriggerEnter (Collider other) {
+		Debug.Log (other.gameObject.name + " has collided with me");
+
+		if (other.gameObject.tag == "Obstacle" || other.gameObject.tag == "Enemy") {
+			gameController.gameOver = true;
+		}
+	}
+}
